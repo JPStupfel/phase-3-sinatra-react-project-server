da51bbe97214b178cb4867ed8e9080159c629701
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-13 17:06:41 -0500

    added 4 defenses for two attacks

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,10 @@ Defense.create name: "posture_up", notes: "Base", result: "closed_guard", stage:
 
 Defense.create name: "shoulder_roll_defense", notes: "Go Early", result: "closed_guard", stage: "mid", attack_id: Attack.find_by(name:'omoplata_classic').id
 
+Defense.create name: "hide_your_arm", notes: "Base", result: "side_control", stage: "mid", attack_id: Attack.find_by(name:'triangle_choke').id
+
+Defense.create name: "pull_arm_out", notes: "Go Early", result: "open_guard", stage: "early", attack_id: Attack.find_by(name:'omoplata_classic').id
+
 
 
 
