2bad4e6c10a24fcf22d655191caf4e01c25de24c
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-18 16:28:50 -0500

    seed data includes closed guard and joined to attacks

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,12 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
-Attack.create name: 'omoplata_classic', result: 'submission', notes: 'shin-em in the adams apple', image: 'https://jiujitsutimes.com/wp-content/uploads/Screenshot-441.png'
+Position.create name:"closed_guard", notes:"hips up. Its all about grip fighting"
 
-Attack.create name: 'triangle_choke', result: 'submission', notes: 'control the head all the while', image: 'https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_1240/MTc0NDA5NTYwODUyMjExMDQ4/scissor-guard-triangle-set-ups-a-bjj-tutorial.webp'
+Attack.create name: 'omoplata_classic', result: 'submission', notes: 'shin-em in the adams apple', image: 'https://jiujitsutimes.com/wp-content/uploads/Screenshot-441.png', position_id: Position.find_by(name: "closed_guard").id
+
+Attack.create name: 'triangle_choke', result: 'submission', notes: 'control the head all the while', image: 'https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_1240/MTc0NDA5NTYwODUyMjExMDQ4/scissor-guard-triangle-set-ups-a-bjj-tutorial.webp',
+position_id: Position.find_by(name: "closed_guard").id
 
 Defense.create name: "posture_up", notes: "Base", result: "closed_guard", stage: "early", attack_id: Attack.find_by(name:'triangle_choke').id
 
