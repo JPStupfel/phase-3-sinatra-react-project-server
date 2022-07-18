64b0c9071388097a37aa4fc36747c0ce0178a79a
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-18 16:23:37 -0500

    Position man and attack map are joined:

--- app/models/Attack.rb
+++ app/models/Attack.rb
@@ -2,4 +2,5 @@ require_relative '../../config/environment.rb'
 
 class Attack < ActiveRecord::Base
     has_many :defense
+    belongs_to :position
 end
---
+++ app/models/Position.rb
@@ -0,0 +1,5 @@
+require_relative '../../config/environment.rb'
+
+class Position < ActiveRecord::Base
+    has_many :attacks
+end
