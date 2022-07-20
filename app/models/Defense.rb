0c42a61d45a0b0d3b2f03f23c52b1b4314597cd5
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-20 15:34:21 -0500

    Defense has create_with_sequence method

--- app/models/Defense.rb
+++ app/models/Defense.rb
@@ -3,4 +3,22 @@ require_relative '../../config/environment.rb'
 class Defense < ActiveRecord::Base
     belongs_to :attack
     has_many :sequences
+
+    def self.create_with_sequence args
+        self.create args
+
+        Sequence.all.each do |sequence|
+            sequence.destroy
+        end
+
+        Sequence.generate
+     end
+  
+
+
 end
+
+# def self.create_with_sequence args
+#     self.create args
+#     sequence.create 
+#  end
--- app/models/Position.rb
+++ app/models/Position.rb
@@ -4,4 +4,10 @@ class Position < ActiveRecord::Base
     has_many :attacks
     has_many :sequences
 
+    def self.create_with_sequence args
+       self.create args
+    end
+
+
+
 end
