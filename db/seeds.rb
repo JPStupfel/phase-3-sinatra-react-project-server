742edd15fc2904c4c771a3953cb0319f4312f315
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-21 13:20:11 -0500

    seed file calls Sequence.generate

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,37 +43,7 @@ Defense.create name: "shrimp_out", attack_id: Attack.find_by(name:'over_the_leg_
 Defense.create name: "pull_closed_guard", attack_id: Attack.find_by(name:'over_the_leg_guard_pass').id, result: "closed_guard", notes: "track the bicep, grab your thigh"
 
 
-# Seed your database here
-
-# Position.create name:"closed_guard", notes:"hips up. Its all about grip fighting"
-# Position.create name:"open_guard", notes:"hips up. Its all about grip fighting"
-# Position.create name:"half_guard", notes:"hips up. Its all about grip fighting"
-# Position.create name:"side_control", notes:"hips up. Its all about grip fighting"
-# Position.create name:"mount", notes:"hips up. Its all about grip fighting"
-# Position.create name:"knee_on_belly", notes:"hips up. Its all about grip fighting"
-# Position.create name:"north_south", notes:"hips up. Its all about grip fighting"
-
-
-
-
-
-
-
-# Attack.create name: 'omoplata_classic', result: 'submission', notes: 'shin-em in the adams apple', image: 'https://jiujitsutimes.com/wp-content/uploads/Screenshot-441.png', position_id: Position.find_by(name: "closed_guard").id
-
-# Attack.create name: 'triangle_choke', result: 'submission', notes: 'control the head all the while', image: 'https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_1240/MTc0NDA5NTYwODUyMjExMDQ4/scissor-guard-triangle-set-ups-a-bjj-tutorial.webp',
-# position_id: Position.find_by(name: "closed_guard").id
-
-# Defense.create name: "posture_up", notes: "Base", result: "closed_guard", stage: "early", attack_id: Attack.find_by(name:'triangle_choke').id
-
-# Defense.create name: "shoulder_roll_defense", notes: "Go Early", result: "closed_guard", stage: "mid", attack_id: Attack.find_by(name:'omoplata_classic').id
-
-# Defense.create name: "hide_your_arm", notes: "Base", result: "side_control", stage: "mid", attack_id: Attack.find_by(name:'triangle_choke').id
-
-# Defense.create name: "pull_arm_out", notes: "Go Early", result: "open_guard", stage: "early", attack_id: Attack.find_by(name:'omoplata_classic').id
-
-
-# Sequence.generate
+ Sequence.generate
 
 
 
